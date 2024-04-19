ba633c3c22d17b1319ba4256389a209cff519658
linjianhui <linjianhui12263@apexsoft.com.cn>
2024-04-19 14:31:38 +0800

    解决react demo找不到easy-dnd问题

--- examples/react-demo/vite.config.ts
+++ examples/react-demo/vite.config.ts
@@ -1,7 +1,13 @@
+import { join } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react() ],
+  resolve: {
+    alias: {
+      'easy-dnd': join(__dirname, '../../build')
+    }
+  }
 })
